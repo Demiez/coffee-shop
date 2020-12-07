65946eac040009f475e041d5a832973712d3a0e3
Dmytro Petrenko <dmg.petrenko@gmail.com>
2020-12-07 10:16:45 +0200

    service and logic

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,33 +8,38 @@ import {
   HttpStatus,
   Patch,
   Delete,
+  Query,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavors')
+  constructor(private readonly coffeesService: CoffeesService) {}
+
+  @Get()
   @HttpCode(HttpStatus.OK)
-  findAll() {
-    return 'This action returns all coffees';
+  findAll(@Query() paginationQuery) {
+    // const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns #[${id}] coffee`;
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
   create(@Body() body) {
-    return body;
+    return this.coffeesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `This action updates #[${id}] coffee`;
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This action removes #[${id}] coffee`;
+    return this.coffeesService.remove(id);
   }
 }
